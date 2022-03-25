7a403a0fa4686c10caf93b59387f1bf4763a41cb
oded <oded86@gmail.com>
2022-03-25 19:16:51 +0300

    lat lng

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='PArking Alerts')
+    return render_template('index.html', title='Parking Alerts')
 
 
 app.run(host='0.0.0.0', port=8040)
