68fe910c75bc32f138acf379a9297eb04819239f
ODED BAHAR <ODED.BAHAR@amadeus.com>
2022-03-27 14:14:14 +0300

    fixed method

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ app.config["IMAGE_UPLOADS"] = os.path.dirname(os.path.realpath(__file__)) + "\\s
 # app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG"]
 
 
-@app.route('/')
-@app.route('/index')
-def upload_image():
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
     if request.method == "POST":
         image = request.files['file']
 
@@ -20,12 +20,12 @@ def upload_image():
             print("Image must have a file name")
             return redirect(request.url)
 
-        #filename = secure_filename(image.filename)
+        # filename = secure_filename(image.filename)
         filename = image.filename
         basedir = os.path.abspath(os.path.dirname(__file__))
         image.save(os.path.join(basedir, app.config["IMAGE_UPLOADS"], filename))
 
-        return render_template("index.html", filename=filename)
+        return render_template("index.html", title='Parking Alerts', filename=filename)
 
     return render_template('index.html', title='Parking Alerts')
 
